49fef42b611dd8a68279363248d56aec23186af1
Alex <seiffarthalexander@gmail.com>
2026-07-22 20:29:43 +0200

    v4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bei JEDER Änderung an der App diese Versionsnummer hochzählen (v2 -> v3 -> ...)
-const CACHE_NAME = "opas-haushalt-v3";
+const CACHE_NAME = "opas-haushalt-v4";
 
 const ASSETS = [
   "./",
